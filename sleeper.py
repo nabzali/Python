6eba5388d661a1fce49430cc4b5377d181e2a137
Nabeel Ali <nabeelali_@outlook.com>
2018-04-04 03:19:08 +0100

    new

---
+++ sleeper.py
@@ -0,0 +1,11 @@
+import time
+for i in range(0,3):
+    print("Hello")
+    time.sleep(0.3)
+    print("World")
+    time.sleep(0.3)
+for i in range(0,3):
+    print("Goodbye")
+    time.sleep(0.3)
+    print("Universe")
+    time.sleep(0.3)    
